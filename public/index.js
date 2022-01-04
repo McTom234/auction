33290487b5a1319d051dd655276f66bf43a47369
Jonas Bellmann <jonas.bellmann@ivao.aero>
2022-01-04 11:50:52 +0100

    breaking change - switch to TS - add JWT auth - refactoring auth flow

--- public/index.js
+++ public/index.js
@@ -1,9 +1,35 @@
-socket.on("state", (msg)=>{
-    Array.from(document.getElementById("content").children).forEach((child)=>{
-      if(child.id == msg){
-        child.style.display = "block";
-      }else{
-        child.style.display = "none";
-      }
+
+function showPage(name){
+    Array.from(document.getElementById("content").children).forEach((child) => {
+        if (child.id === name) {
+            child.style.display = "block";
+        } else {
+            child.style.display = "none";
+        }
     })
-  })
+}
+function connectSocket(room){
+    const socket = io({autoConnect: false});
+    const urlParams = new URLSearchParams(window.location.search);
+
+    socket.auth = {token: urlParams.get("t"), type: room};
+    socket.connect();
+
+    socket.on("connect_error", (err) => {
+        console.log("Server returned error during connection: " + JSON.stringify(err));
+        showPage("error");
+    });
+
+    socket.on("state", (msg) => {
+        showPage(msg);
+    });
+
+    socket.on("error", (msg) => {
+        showPage("error");
+    })
+
+    return socket
+}
+
+
+
