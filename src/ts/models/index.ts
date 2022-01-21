e4697bd8020142747cbd229bb39b7249efef2680
Jonas Bellmann <jonas.bellmann@ivao.aero>
2022-01-22 00:36:11 +0100

    add new models

--- src/ts/models/index.ts
+++ src/ts/models/index.ts
@@ -2,3 +2,5 @@ export * from './state';
 export * from './userData';
 export * from './history';
 export * from './auctionSocket';
+export * from './config';
+export * from './product';
---
+++ src/ts/models/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+	name: string
+	description: string
+	image: string
+	minStep: number
+	price: number
+	goal: string | number
+}
