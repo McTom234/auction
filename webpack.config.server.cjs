199222f24696982f9981721c2c356a1e8bc87f27
Jonas Bellmann <jonas.bellmann@ivao.aero>
2022-01-22 00:35:15 +0100

    update products and webpack config

--- webpack.config.server.cjs
+++ webpack.config.server.cjs
@@ -2,7 +2,7 @@ const path = require("path");
 module.exports = {
     target: 'node',
     entry: './src/ts/socket.ts',
-    mode: "production",
+    mode: "development",
     watch: true,
     watchOptions: {
         ignored: /node_modules/,
